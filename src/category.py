f3682f889ed29866f75ea4278480d565463387b9
Denis Khudyakov <denis.hudyakov27@gmail.com>
2023-12-19 23:27:24 +0500

    home work done

--- src/category.py
+++ src/category.py
@@ -1,5 +1,7 @@
 from collections import Counter
 class Category:
+    count_category1: int = 0
+    count_product1: int = 0
 
     def __init__(self, name: str, description: str, products: list) -> None:
         """конструктор класса, категория товаров, атрибуты: название, описание, перечень продуктов"""
@@ -8,6 +10,9 @@ class Category:
         self.products = products
         self.count_category = len(list(filter(lambda prod: prod.quantity_in_stock, self.products)))
         self.count_product = len(set(self.products))
+
+
+
 
 
 
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from src.product import Product
 from src.category import Category
+from src.views import read_json, PATH_FILE
 
 
 
@@ -8,13 +9,20 @@ if __name__ == '__main__':
     prod2 = Product(name="Паштет", description="Мясные изделия", price=100, quantity_in_stock=15)
     prod3 = Product(name="Колбаса", description="Мясные изделия", price=300, quantity_in_stock=5)
     prod4 = Product(name="Сыр", description="Молочные продукты", price=500, quantity_in_stock=9)
-    _list = [prod1, prod2, prod3, prod4]
+    prod5 = Product(name="Сыр", description="Молочные продукты", price=500, quantity_in_stock=0)
+    _list = [prod1, prod2, prod3, prod4, prod5]
     market = Category('Продуктовый', 'Маркетплэйс продуктовых товаров', _list)
+    print(Category('x', 'y', _list).count_category)
     assert market.count_category == 4
-    assert market.count_product == 4
+    assert market.count_product == 5
     assert prod1.name == 'Молоко'
     assert prod4.price == 500
     assert market.name == 'Продуктовый'
+    def initial_class_generator(func):
+        for i in func:
+            yield Category(i['name'], i['description'], [Product(y['name'], y['description'], y['price'], y['quantity']) for y in i['products']])
+
+    print(next(initial_class_generator(read_json(PATH_FILE))))
 
 
 
